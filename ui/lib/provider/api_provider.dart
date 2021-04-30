0b54f82ba80adb8c7f36fa990a1d4e08ea3186f6
Lorran Rodrigues <lorran.rodrigues@elevaeducacao.com.br>
2021-04-30 11:16:56 -0300

    update provider

--- ui/lib/provider/api_provider.dart
+++ ui/lib/provider/api_provider.dart
@@ -5,12 +5,14 @@ import 'package:codas_method/model/codas_input.dart';
 import 'package:codas_method/model/model_results.dart';
 
 class ApiProvider {
-  final String _baseUrl = " https://vprvdbyhz1.execute-api.us-east-2.amazonaws.com/prod"
-  final String _endpoint = "$_baseUrl/codas/";
+  final String _baseUrl =
+      "https://vprvdbyhz1.execute-api.us-east-2.amazonaws.com/prod";
+
   final Dio _dio =
       Dio(BaseOptions(headers: {"Access-Control-Allow-Origin": "*"}));
 
   Future<ModelResults> getRanking(CodasInput input) async {
+    String _endpoint = "${_baseUrl}/codas/";
     try {
       Response response = await _dio.post(_endpoint, data: jsonEncode(input));
       print("success");
